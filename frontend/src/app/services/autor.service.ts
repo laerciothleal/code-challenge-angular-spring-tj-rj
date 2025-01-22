0458bb45bedf8658cb0e7d576d34629312c2f4e3
Laercio  Leal <laercioleal@Laercios-MacBook-Pro.local>
2025-01-22 14:43:00 -0300

    adding some adjustments

--- frontend/src/app/services/autor.service.ts
+++ frontend/src/app/services/autor.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AutorService {
-  private baseUrl = 'http://localhost:8080/api/v1/utores';
+  private baseUrl = 'http://localhost:8080/api/v1/autores';
 
   constructor(private http: HttpClient) {}
 
