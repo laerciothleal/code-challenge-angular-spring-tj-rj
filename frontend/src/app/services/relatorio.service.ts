6bc979104d3d1504282371e6faf67b2a3a34765b
Laercio  Leal <laercioleal@Laercios-MacBook-Pro.local>
2025-02-04 17:31:11 -0300

    adding some adjustments in the view and in the price field

--- frontend/src/app/services/relatorio.service.ts
+++ frontend/src/app/services/relatorio.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export interface Relatorio {
+  id: number;
   autorNome: string;
   livroTitulo: string;
   livroEditora: string;
